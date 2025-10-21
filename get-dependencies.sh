7689e0ea49fd32d19c76ce4ed2b896c261a1fce3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-21 15:38:57 -0400

    Create get-dependencies.sh

---
+++ get-dependencies.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+pacman -Syu --noconfirm \
+	base-devel       \
+  carla            \
+	curl             \
+	git              \
+	lame             \
+	libxcb           \
+	libxcursor       \
+	libxi            \
+	libxkbcommon     \
+	libxkbcommon-x11 \
+	libxrandr        \
+	libxtst          \
+	lmms             \
+	pipewire-audio   \
+	pulseaudio       \
+	pulseaudio-alsa  \
+	qt5ct            \
+	qt5-wayland      \
+	wget             \
+	xorg-server-xvfb \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-mesa --prefer-nano opus-mini
+
+pacman -Q lmms | awk '{print $2; exit}' > ~/version
