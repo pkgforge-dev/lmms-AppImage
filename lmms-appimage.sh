91675f97a62d48c89452456b555f6c82f518b9a4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-21 15:45:21 -0400

    Create lmms-appimage.sh

---
+++ lmms-appimage.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+VERSION="$(cat ~/version)"
+
+export ADD_HOOKS="self-updater.bg.hook"
+export ICON=/usr/share/icons/hicolor/scalable/apps/lmms.svg
+export DESKTOP=/usr/share/applications/lmms.desktop
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export DEPLOY_PIPEWIRE=1
+export DEPLOY_OPENGL=1
+export OUTNAME=lmms-"$VERSION"-anylinux-"$ARCH".AppImage
+
+# Deploy dependencies
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun \
+	/usr/bin/lmms    \
+	/usr/lib/lmms/*  \
+	/usr/lib/lmms/*/*
+
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
+
+mkdir -p ./dist
+mv -v ./*.AppImage* ./dist
+mv -v ~/version     ./dist
