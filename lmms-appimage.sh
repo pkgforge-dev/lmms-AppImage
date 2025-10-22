1ed5852cff9b650f643361793d11712b63f499b8
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-22 17:12:10 -0400

    deploy carla manually

--- lmms-appimage.sh
+++ lmms-appimage.sh
@@ -13,6 +13,7 @@ export DESKTOP=/usr/share/applications/lmms.desktop
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DEPLOY_PIPEWIRE=1
 export DEPLOY_OPENGL=1
+export DEPLOY_PYTHON=1
 export OUTNAME=lmms-"$VERSION"-anylinux-"$ARCH".AppImage
 
 # Deploy dependencies
@@ -20,8 +21,21 @@ wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun /usr/bin/lmms -- --allowroot
 ./quick-sharun \
-	/usr/lib/lmms/*  \
-	/usr/lib/lmms/*/*
+	/usr/lib/lmms/*    \
+	/usr/lib/lmms/*/*  \
+	/usr/lib/carla/*   \
+	/usr/lib/carla/*/*
+
+# TODO remove me once quick-sharun add carla deployment
+cp -r /usr/share/carla ./AppDir/share
+cp -rn /usr/lib/carla  ./AppDir/lib
+
+echo "Sharunning the carla binaries..."
+bins_to_find="$(find ./AppDir/lib/carla -exec file {} \; | grep -i 'elf.*executable' | awk -F':' '{print $1}')"
+for plugin in $bins_to_find; do
+	mv -v "$plugin" ./AppDir/shared/bin && ln -f ./AppDir/sharun "$plugin"
+	echo "Sharan $plugin"
+done
 
 # MAKE APPIMAGE WITH URUNTIME
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
