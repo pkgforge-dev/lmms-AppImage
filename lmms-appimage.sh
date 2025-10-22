74ae14f70b51bb05f3c51a7f84e44906433c6887
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-22 19:26:31 -0400

    carla has been dealt with

--- lmms-appimage.sh
+++ lmms-appimage.sh
@@ -20,28 +20,9 @@ export OUTNAME=lmms-"$VERSION"-anylinux-"$ARCH".AppImage
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun \
-	/usr/bin/lmms          \
-	/usr/lib/lmms/*        \
-	/usr/lib/lmms/*/*      \
-	/usr/bin/carla*        \
-	/usr/lib/carla/*       \
-	/usr/lib/carla/jack/*  \
-	/usr/lib/carla/styles/*
-
-# TODO remove me once quick-sharun add carla deployment
-cp -r /usr/share/carla ./AppDir/share
-cp -rn /usr/lib/carla  ./AppDir/lib
-
-sed -i \
-	-e 's|INSTALL_PREFIX="/usr"|INSTALL_PREFIX="$APPDIR"|' \
-	./AppDir/lib/carla/carla-bridge-lv2-modgui ./AppDir/bin/carla*
-
-echo "Sharunning the carla binaries..."
-bins_to_find="$(find ./AppDir/lib/carla -exec file {} \; | grep -i 'elf.*executable' | awk -F':' '{print $1}')"
-for plugin in $bins_to_find; do
-	mv -v "$plugin" ./AppDir/shared/bin && ln -f ./AppDir/sharun "$plugin"
-	echo "Sharan $plugin"
-done
+	/usr/bin/lmms     \
+	/usr/lib/lmms/*   \
+	/usr/lib/lmms/*/*
 
 # MAKE APPIMAGE WITH URUNTIME
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
