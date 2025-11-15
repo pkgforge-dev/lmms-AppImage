3fa5d31d21601acbfe43471c9b80af4b96368f58
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-15 04:37:44 -0400

    simpify

--- lmms-appimage.sh
+++ lmms-appimage.sh
@@ -3,45 +3,38 @@
 set -eux
 
 ARCH="$(uname -m)"
-URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-VERSION="$(cat ~/version)"
 
+export VERSION="$(cat ~/version)"
 export ADD_HOOKS="self-updater.bg.hook"
 export ICON=/usr/share/icons/hicolor/scalable/apps/lmms.svg
 export DESKTOP=/usr/share/applications/lmms.desktop
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DEPLOY_PIPEWIRE=1
 export DEPLOY_OPENGL=1
+export DEPLOY_SYS_PYTHON=1
 export EXEC_WRAPPER=1
-export OUTNAME=lmms-"$VERSION"-anylinux-"$ARCH".AppImage
+export OUTPATH=./dist
 
 # Deploy dependencies
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun \
 	/usr/bin/lmms             \
+	/usr/lib/lmms             \
 	/usr/lib/lmms/*           \
 	/usr/lib/lmms/*/*         \
-	/usr/bin/python*          \
 	/usr/bin/carla*           \
 	/usr/lib/carla/*          \
+	/usr/lib/carla            \
+	/usr/share/carla          \
 	/usr/lib/carla/jack/*     \
 	/usr/lib/carla/styles/*   \
 	/usr/lib/alsa-lib/*jack*  \
 	/usr/lib/alsa-lib/*pulse*
 
-# lmms lib dir contains more than just libraries
-cp -rn /usr/lib/lmms ./AppDir/lib
-
 # wrap RemoteZynAddSubFx with sharun
 ln -f ./AppDir/sharun ./AppDir/lib/lmms/RemoteZynAddSubFx
-
-# carla needs a matching pyqt5 version to work
-# so we need to bundle it manually
-cp -r /usr/share/carla ./AppDir/share
-cp -rn /usr/lib/carla  ./AppDir/lib
-cp -r /usr/lib/python3.* ./AppDir/lib
 
 sed -i \
 	-e 's|INSTALL_PREFIX="/usr"|INSTALL_PREFIX="$APPDIR"|g' \
@@ -60,10 +53,4 @@ for plugin in $bins_to_find; do
 done
 
 # MAKE APPIMAGE WITH URUNTIME
-wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
-chmod +x ./uruntime2appimage
-./uruntime2appimage
-
-mkdir -p ./dist
-mv -v ./*.AppImage* ./dist
-mv -v ~/version     ./dist
+./quick-sharun --make-appimage
