21da877b425828b0b9cdd30de617543cdc82832e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-20 07:14:13 -0400

    should not be needed anymore

--- lmms-appimage.sh
+++ lmms-appimage.sh
@@ -22,10 +22,7 @@ chmod +x ./quick-sharun
 ./quick-sharun \
 	/usr/bin/lmms             \
 	/usr/lib/lmms             \
-	/usr/lib/lmms/*           \
-	/usr/lib/lmms/*/*         \
 	/usr/bin/carla*           \
-	/usr/lib/carla/*          \
 	/usr/lib/carla            \
 	/usr/share/carla          \
 	/usr/lib/carla/jack/*     \
@@ -33,24 +30,10 @@ chmod +x ./quick-sharun
 	/usr/lib/alsa-lib/*jack*  \
 	/usr/lib/alsa-lib/*pulse*
 
-# wrap RemoteZynAddSubFx with sharun
-ln -f ./AppDir/sharun ./AppDir/lib/lmms/RemoteZynAddSubFx
-
 sed -i \
 	-e 's|INSTALL_PREFIX="/usr"|INSTALL_PREFIX="$APPDIR"|g' \
 	-e 's|which python3|command -v python3|g'               \
 	./AppDir/lib/carla/carla-*-modgui ./AppDir/bin/carla*
-
-echo "Sharunning the carla binaries..."
-bins_to_find="$(find ./AppDir/lib/carla \
-	-exec file {} \; | grep -i 'elf.*executable' | awk -F':' '{print $1}')"
-for plugin in $bins_to_find; do
-	if [ -x "$plugin" ]; then
-		mv -v "$plugin" ./AppDir/shared/bin
-		ln -f ./AppDir/sharun "$plugin"
-		echo "Sharan $plugin"
-	fi
-done
 
 # MAKE APPIMAGE WITH URUNTIME
 ./quick-sharun --make-appimage
