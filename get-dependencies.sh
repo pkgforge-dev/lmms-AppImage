0035116654e49ecdb122eef9d93198b1e4956ce3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-14 19:43:44 -0400

    use common packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -34,7 +34,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-mesa --prefer-nano opus-mini
+./get-debloated-pkgs.sh --add-common --prefer-nano
 
 echo "Building lmms..."
 echo "---------------------------------------------------------------"
