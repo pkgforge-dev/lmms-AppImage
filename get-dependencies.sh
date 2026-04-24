39d72f89653ea99272d735057a2eb7ade16e2db1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-24 13:07:02 -0400

    fix?

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,6 +8,7 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
 	carla            \
+	libsamplerate    \
 	sndio            \
 	suil             \
 	pipewire-audio   \
